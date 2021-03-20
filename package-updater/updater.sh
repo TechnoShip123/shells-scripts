a84b8f89849b61ad0db7ad0fe01d1f80483ce5b1
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2021-03-20 18:05:10 -0500

    Fixed updater.sh

--- package-updater/updater.sh
+++ package-updater/updater.sh
@@ -6,7 +6,7 @@ echo "Welcome to TechnoShip123's package updater! This uses the 'Advanced Packag
 sleep 1
 echo "Would you like to update your packages? I will be using apt update, upgrade, and autoremove."
 
-select ync in "Yes" "No" "Info"; do
+select yni in "Yes" "No" "Info"; do
 	case $yni in
 		Yes ) sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt autoremove -y; echo $'\nYour packages are now updated, goodbye!'; break;;
 		No ) echo "No problem, exiting the shell script..."; exit 0;;
