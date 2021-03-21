43c554df88358abc88ad4faa0827ecf04d9d3282
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2021-03-20 19:02:51 -0500

    Update updater.sh

--- package-updater/updater.sh
+++ package-updater/updater.sh
@@ -4,13 +4,17 @@ figlet Package Updater!
 
 echo "Welcome to TechnoShip123's package updater! This uses the 'Advanced Package Tool', AKA 'apt'."
 sleep 1
-echo "Would you like to update your packages? I will be using apt update, upgrade, and autoremove."
+echo "In this script, I will be using apt update, upgrade, and autoremove."
+sleep 1
 
-select yni in "Yes" "No" "Info"; do
-	case $yni in
-		Yes ) sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt autoremove -y; echo $'\nYour packages are now updated, goodbye!'; break;;
-		No ) echo "No problem, exiting the shell script..."; exit 0;;
-		# This option does not exit the script:
-		Info ) echo "This shell script was made by TechnoShip123 (Thonk)."; echo "You can find the GitHub repository at 'https://github.com/TechnoShip123/shell-scripts'!"
-	esac
-done
+read -p "Would you like to update your packages? (y/n) " -n 1 -r
+echo   # (Blank Line)
+if [[ $REPLY =~ ^[Yy]$ ]]  # Check if the reply is yes (upper and lowercase are allowed).]
+then
+	echo "Okay, running updater..."; sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt autoremove -y]
+	echo; echo "Everything has been updated! Press any key to exit."; read -p ""
+elif [[ $REPLY =~ ^[Nn]$ ]] # If they said no.
+then
+	echo "Alright, exiting the script. Goodbye!"
+	exit 0
+fi
