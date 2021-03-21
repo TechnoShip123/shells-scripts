020a7b721199f1c108d6efdb38ca5b297ef974b1
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2021-03-20 19:49:36 -0500

    Completed updater.sh
    Minor fixes and additions, it is now complete.

--- package-updater/updater.sh
+++ package-updater/updater.sh
@@ -11,9 +11,10 @@ read -p "Would you like to update your packages? (y/n) " -n 1 -r
 echo   # (Blank Line)
 if [[ $REPLY =~ ^[Yy]$ ]]  # Check if the reply is yes (upper and lowercase are allowed).]
 then
-	echo "Okay, running updater..."; sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt autoremove -y]
+	echo "Okay, running updater..."; sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y
 	echo; echo "Everything has been updated! Press any key to exit."; read -p ""
-elif [[ $REPLY =~ ^[Nn]$ ]] # If they said no.
+
+elif [[ $REPLY =~ ^[Nn]$ ]] # If the reply is no.
 then
 	echo "Alright, exiting the script. Goodbye!"
 	exit 0
