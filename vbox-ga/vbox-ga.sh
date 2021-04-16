9c4c2f5e7e8fbe4881dfa0c3efe0f4bb4a25b6c2
Thonk <75491816+TechnoShip123@users.noreply.github.com>
2021-04-16 07:38:17 -0500

    Create vbox-ga.sh

---
+++ vbox-ga/vbox-ga.sh
@@ -0,0 +1,15 @@
+#!usr/bin/env bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install build-essential dkms linux-headers-$(uname -r)
+
+read -n 1 -s -r -p "Make sure to insert the Guest Additions CD. If it is inserted/Once it is inserted, press any key to continue..."
+
+sudo mkdir -p /mnt/cdrom
+sudo mount /dev/cdrom /mnt/cdrom
+
+cd /mnt/cdrom
+sudo sh ./VBoxLinuxAdditions.run --nox11
+
+read -n 1 -s -r -p "All set! I'll be restarting the VM now, press any key to continue..."
+sudo reboot now
